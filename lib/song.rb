98ff7385e6508257e3e8a401e7589d856e02525e
April Adams <illusionlace@gmail.com>
2020-10-05 23:34:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,28 @@
-class Song
-  attr_accessor :name, :artist, :genre
-  @@count = 0
-  @@genres = []
-  @@artists = []
-
-  def initialize(song_name, artist, genre)
-    @name = song_name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@artists << artist
-    @@genres << genre
-end
-def self.count
-  @@count
-end
-def self.artists
-  @@artists.uniq
-end
-def self.genres
-  @@genres.uniq
-end
-def self.genre_count
-  genre_count = Hash.new(0)
-  @@genres.each {|genre| genre_count[genre] += 1}
-  genre_count
-end
-def self.artist_count
-  artist_count = Hash.new(0)
-  @@artists.each {|artist| artist_count[artist] += 1}
-  artist_count
-end
-end
+class TicTacToe 
+  def initialize(board=nil)
+    @board = Array.new(9, " ")
+  end 
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+def display_board
+  puts " #{@board[0]} " + "|" + " #{@board[1]} " + "|" + " #{@board[2]} "
+  puts "------------"
+  puts " #{@board[3]} " + "|" + " #{@board[4]} " + "|" + " #{@board[5]} "
+  puts "------------"
+  puts " #{@board[6]} " + "|" + " #{@board[7]} " + "|" + " #{@board[8]} "
+end 
+def input_to_index(input)
+  input.to_i - 1
+end 
+def move(index, value)
+  @board[index] = value 
+  @board 
+end 
